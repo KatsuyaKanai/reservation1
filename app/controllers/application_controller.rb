39c41b43f207c17af1d8aee54026386a569cb10a
KatsuyaKanai <kanaisilvia@yahoo.co.jp>
2022-07-11 16:47:46 +0900

    gitignore

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,10 +12,6 @@ class ApplicationController < ActionController::Base
     def after_sign_out_path_for(resource)
         '/homes/index'
       end
-    def after_logout_path_for(resource)
-        binding.pry
-        '/homes/index'
-    end
     protected
 
   def configure_permitted_parameters
